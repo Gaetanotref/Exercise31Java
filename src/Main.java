554f61621b3e8253b0625bf18596d006a4035c51
Gaetano Trefiletti <gaetano.trefiletti.gt@gmail.com>
2022-12-12 14:10:03 +0100

    Commit init

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        String[] array1 ={" I want "," to learn "," how to code! "};
+        String[] array2 ={"I want","to learn","how to code!"};
+
+        int statement1 = array2[0].compareTo("I want");
+        int statement2 = array2[1].compareToIgnoreCase("To learn");
+
+        boolean result = !(statement1>statement2);
+        System.out.println(result);
+    }
+}
