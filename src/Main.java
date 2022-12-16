345cd7964af1f633a002e284f9c9e44adc033f61
Gaetano Trefiletti <gaetano.trefiletti.gt@gmail.com>
2022-12-16 16:16:12 +0100

    Correction

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         String[] array1 ={" I want "," to learn "," how to code! "};
-        String[] array2 ={"I want","to learn","how to code!"};
+        String[] array2 ={array1[0].trim(),array1[1].trim(),array1[2].trim()};
 
         int statement1 = array2[0].compareTo("I want");
         int statement2 = array2[1].compareToIgnoreCase("To learn");
